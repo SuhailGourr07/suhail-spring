61160cd66d40be945015ef4c41d0506a86d8f673
Mohd Suhail <suhailshoib519@gmail.com>
2026-08-12 12:39:51 +0530

    Initial commit

---
+++ src/main/java/learning/spring/Main.java
@@ -0,0 +1,25 @@
+package learning.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import learning.spring.entity.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		
+		Employee employee1 = ioc.getBean("emp1", Employee.class);
+		
+		System.out.println(employee1);
+		
+		
+        Employee employee2 = ioc.getBean("emp2", Employee.class);
+		
+		System.out.println(employee2);
+		
+	}
+
+}
