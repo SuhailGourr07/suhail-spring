2d9abe108b4999312c80011b537f7346aadd8fa1
Mohd Suhail <suhailshoib519@gmail.com>
2026-08-22 18:47:38 +0530

    update

--- src/main/java/learning/spring/Main.java
+++ src/main/java/learning/spring/Main.java
@@ -3,7 +3,6 @@ package learning.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import learning.spring.entity.Address;
 import learning.spring.entity.Employee;
 
 public class Main {
@@ -13,17 +12,9 @@ public class Main {
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
 		
 		Employee employee1 = ioc.getBean("emp1", Employee.class);
-		Address address1 = ioc.getBean("add1", Address.class);
 		
 		System.out.println(employee1);
-		System.out.println(address1);
 		
-		
-		Employee employee2 = ioc.getBean("emp2", Employee.class);
-		System.out.println(employee2);
-		
-		Address address2 = ioc.getBean("add2", Address.class);
-		System.out.println(address2);
 	}
 
 }
