db844c079750304bff5f5558bc0998fd23893c2e
Mohd Suhail <suhailshoib519@gmail.com>
2026-08-24 19:55:18 +0530

    update

--- src/main/java/learning/spring/Main.java
+++ src/main/java/learning/spring/Main.java
@@ -1,9 +1,7 @@
 package learning.spring;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import learning.spring.entity.Address;
 import learning.spring.entity.Employee;
 
 public class Main {
@@ -13,22 +11,11 @@ public class Main {
 		ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
 		
 		Employee employee1 = ioc.getBean("emp1", Employee.class);
-//		Address address1 = ioc.getBean("add1", Address.class);
 		
 		System.out.println(employee1);
 		
 		ioc.registerShutdownHook();
 		
-//		System.out.println(address1);
-		
-		
-		/*
-		 * Employee employee2 = ioc.getBean("emp2", Employee.class);
-		 * System.out.println(employee2);
-		 * 
-		 * Address address2 = ioc.getBean("add2", Address.class);
-		 * System.out.println(address2);
-		 */
 	}
 
 }
